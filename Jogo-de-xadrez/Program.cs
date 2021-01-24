a9f0f947d809a6c3e07c9a3ec6b4ac21dd3b900c
MARCELO073 <marcelonogueira072@gmail.com>
2021-01-24 04:40:06 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- Jogo-de-xadrez/Program.cs
+++ Jogo-de-xadrez/Program.cs
@@ -7,11 +7,12 @@ namespace Jogo_de_xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine("Posicao: " + P);
+            
+     
             Tabuleiro tab = new Tabuleiro(8,8);
 
+            Tela.imprimirTabuleiro(tab);
+            
             Console.ReadKey();
         }
     }
--- Jogo-de-xadrez/Tabuleiro/Tabuleiro.cs
+++ Jogo-de-xadrez/Tabuleiro/Tabuleiro.cs
@@ -6,17 +6,22 @@ namespace Jogo_de_xadrez
 {
     class Tabuleiro
     {
-        public int linha { get; set; }
-        public int colunha { get; set; }
-        private Peca [,] pecas;
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
 
-        public Tabuleiro(int linhas, int colunhas)
+        public Tabuleiro(int linhas, int colunas)
         {
-            this.linha = linha;
-            this.colunha = colunha;
-            pecas = new Peca [linhas, colunhas];
-        }
-        
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        } 
+
+        public Peca peca(int linhas,int colunas)
+        {
+            return pecas[linhas, colunas];
+        } 
+
 
 
     }
