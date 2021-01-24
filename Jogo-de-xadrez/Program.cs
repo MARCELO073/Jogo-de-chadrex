687e7f1afa70c3d6bdd581f68a69801ef20d09d3
MARCELO073 <marcelonogueira072@gmail.com>
2021-01-24 02:58:52 -0300

    Criando classe Posicao

--- Jogo-de-xadrez/Program.cs
+++ Jogo-de-xadrez/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using Jogo_de_xadrez.Tabuleiro;
 namespace Jogo_de_xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Jogo-de-xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jogo_de_xadrez.Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get;  set; }
+        public int coluna  { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
